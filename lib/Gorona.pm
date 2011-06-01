9e1f6f9fbccf4818e3d6482c8b06cf809c448c1e
Scott Walters <scott@slowass.net>
2011-06-01 16:14:51 -0400

    Desc of what it is for the docs.

--- lib/Gorona.pm
+++ lib/Gorona.pm
@@ -13,7 +13,7 @@ __END__
 
 =head1 NAME
 
-Gorona - Coro based PSGI web server... for Gopher
+Gorona - Coro based PSGI web server... for Gopher (with apologies to Miyagawa and his Corona)
 
 =head1 SYNOPSIS
   
@@ -21,9 +21,18 @@ Gorona - Coro based PSGI web server... for Gopher
 
 =head1 DESCRIPTION
 
-XXX todo: use L<Coro::AIO> (and L<IO::AIO>) if available, to send the static filehandle using sendfile(2).
+Gopher has long been the preferred hypertext protocol, offering rich media, metadata annotation,
+rich forms, accessibility, high performance, fantastic content, and a lack of utterly bullshit content
+such as L<http://myspace.com>, but has thus far been lacking is a generic stack for mounting 
+dynamic applications.
+Enter L<Gorona>, the first L<Rack> inspired server for Gopher.
 
-XXX todo:  set C<psgi.multithread> env var on.
+Since Gopher is essentially a superset of HTML/HTTP except for the stupid parts, goronaiaing up 
+your existing Web app should just work (note:  JavaScript support is not implemented at this time).
+
+Uses L<Coro::AIO> (and L<IO::AIO>) if available, to send the static filehandle using C<sendfile(2)>.
+
+Sets C<psgi.multithread> env var on.
 
 =head1 HISTORY
 
@@ -43,3 +52,4 @@ at your option, any later version of Perl 5 you may have available.
 
 =head1 SEE ALSO
 
+A professional.
